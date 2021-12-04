97a52fc46c5c09f44763df4fd66953668564e9ab
benasmith1 <81334348+benasmith1@users.noreply.github.com>
2021-12-03 18:29:46 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ const app = express();
 app.use(express.json());
 app.use(parser.json() );
 app.use(parser.urlencoded({ extended: true })); 
-app.use(express.static(__dirname + '/public_html')); //__dirname
+app.use(express.static(__dirname + '/public_html')); 
+app.use(express.static(__dirname + '/public_html' + '/tiktakto')); 
+app.use(express.static(__dirname + '/public_html' + '/marketplace')); 
+app.use(express.static(__dirname + '/public_html' + '/tamagachi')); 
+
+
+
+
 //app.use(express.static(__dirname));
 
 
@@ -52,26 +59,56 @@ app.use(sessions({
 
 
 
+var Schema = mongoose.Schema;
+
+var itemSchema = new Schema({ 
+    type: String,
+    user: String, 
+});
+
+var userSchema = new Schema({ 
+    name: String,
+    items: String,
+});
+
+var item = mongoose.model('items', itemSchema );
+var user = mongoose.model('users', userSchema );
+
+
+
+
+
 app.get('/market', (req, res) => {
     console.log('/market')
     session=req.session;
     session.username = 'bena' // REPLACE W /login/user, req.body.username
-    res.sendFile(__dirname + "/public_html/marketplace.html");
-
-
+    res.sendFile(__dirname + "/public_html/marketplace/marketplace.html");
 });
 
 
+app.get('/tamagachi', (req, res) => {
+    console.log('/tamagachi')
+    session=req.session;
+    session.username = 'bena' // REPLACE W /login/user, req.body.username
+    res.sendFile(__dirname + "/public_html/tamagachi/tamagachi.html");
+});
+
+app.get('/tiktaktoe', (req, res) => {
+    console.log('/tiktaktoe')
+    session=req.session;
+    session.username = 'bena' // REPLACE W /login/user, req.body.username
+    res.sendFile(__dirname + "/public_html/tiktakto/tiktaktoe.html");
+});
 
 
-app.get('/get/my/items/', async function (req, res) {
-    //gets the full item list from the database sends this to the client
+app.get('/get/my/book/', async function (req, res) {
+    //gets the my item list from the database sends this to the client
     var rtn = '';
     var lst = [];
     var user = session.username
     var usr = await db.collection("users").findOne({name: user});
     console.log('usr is: '+ usr)
-    var itm = usr.items
+    var itm = usr.book
     
     console.log('itm is: '+ itm)
     itm = [itm]
@@ -83,8 +120,8 @@ app.get('/get/my/items/', async function (req, res) {
 
 
 
-app.get('/get/items/', async function (req, res) {
-    //gets the full item list from the database sends this to the client
+app.get('/get/books/', async function (req, res) {
+    //gets the full item list without my items from the database sends this to the client
     var rtn = '';
     var lst = [];
 
@@ -108,6 +145,7 @@ app.get('/get/items/', async function (req, res) {
 
 
 app.post('/do/trade/', async function (req, res) {
+    console.log('this version reached!')
     const othrsitm = req.body.type;
     const otherusr = req.body.user;
 
@@ -118,7 +156,7 @@ app.post('/do/trade/', async function (req, res) {
     myusername = session.username
     console.log('myusername is: ' + myusername)
     var myuser = await db.collection('users').findOne({name: myusername});
-    var myitem = myuser.items;
+    var myitem = myuser.book;
 
     console.log('my item is: ' + myitem)
     console.log('myusermae: ' + myusername)
@@ -132,11 +170,11 @@ app.post('/do/trade/', async function (req, res) {
 
     db.collection('users').updateOne(
         { name: myusername},
-        { $set: {items: othrsitm}}
+        { $set: {book: othrsitm}}
     );
     db.collection('users').updateOne(
         { name: otherusr},
-        { $set: {items: myitem}}
+        { $set: {book: myitem}}
     );
     db.collection('items').updateOne(
         { user: myusername},
@@ -150,7 +188,97 @@ app.post('/do/trade/', async function (req, res) {
     }
 });
 
+app.post('/tamagachi/timer/', async function (req, res){
+    console.log('tamagachi timer server save reached');
+    user = session.username;
+    data = req.query;
+    foodtimerwhenleft = data.foodtimerwhenleft;
+    playtimerwhenleft = data.playtimerwhenleft;
+    console.log(req);
+    console.log('foodtimerwhenleft' + foodtimerwhenleft);
+    console.log('playtimerwhenleft' + playtimerwhenleft);
 
-app.listen(port, function(){
+    timeexited = data.timeexited;
+    updatestring = {'tamagachifoodtimer': foodtimerwhenleft, 'tamagachiplaytimer': playtimerwhenleft,'timeexitedtamagachi': timeexited };
+    
+    console.log('updatastring is: ' + updatestring);
+    console.log('updatastring tamagachifoodtimer is: ' + updatestring.tamagachifoodtimer);
+    console.log('updatastring is tamagachiplaytimer: ' + updatestring.tamagachiplaytimer);
+    console.log('timeexitedtamagachi : ' + updatestring.timeexitedtamagachi);
+
+
+
+    db.collection('users').updateOne(
+        { name: user},
+        { $set: {'tamagachitimer' : updatestring}} // need to index into object
+    );
+
+    /* db.collection('users').updateOne(
+        { name: user},
+        { $set: {gamescores: {tamagachiplaytimer: playtimerwhenleft}}} // need to index into object
+    );
+    db.collection('users').updateOne(
+        { name: user},
+        { $set: {gamescores: {timeexitedtamagachi: timeexited}}} // need to index into object
+    ); */
+
+})
+
+app.get('/tamagachi/my/timer/', async function (req, res){
+    console.log('made it to tamagachi get');
+    user = session.username;
+
+
+    var item = await db.collection("users").findOne({"name": user});
+    console.log(item);
+
+    console.log('tamagachi timer is:');
+    console.log(item.tamagachitimer)
+    mytamtimer = JSON.stringify(item.tamagachitimer)
+
+
+    res.end(mytamtimer)
+
+
+});
+
+
+
+
+app.get('/load/game/data', async function (req, res){
+    session=req.session
+    myusername = session.username
+
+    var user = await db.collection("users").findOne({"name": user});
+    console.log(data);
+
+    console.log('my data is:');
+    console.log(user.mydata)
+    mydata = JSON.stringify(user.mydata)
+
+
+    res.end(mydata)
+
+
+});
+
+app.post('/save/game/', async function (req, res) {
+    //gets the full item list without my items from the database sends this to the client
+
+    session=req.session
+    myusername = session.username
+    mydata = req.body.params
+
+    db.collection('users').updateOne(
+        { name: user},
+        { $set: {'mydata' : mydata}} // need to index into object
+    );
+});
+
+
+
+
+app.listen(port, async function(){
     console.log(`App listening at ' + ${port}`);
   });
+
